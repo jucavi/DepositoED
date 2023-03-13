e91c3f84401667a57b3c368ce083482e58cbd68e
Juan carlos Vilarrubia <jcarvil@gmail.com>
2023-03-13 08:50:23 +0100

    Documentada la clase CCuenta

--- src/dam_ed04_actividad/cuentas/CCuenta.java
+++ src/dam_ed04_actividad/cuentas/CCuenta.java
@@ -5,21 +5,48 @@
 
 package dam_ed04_actividad.cuentas;
 
-
+/**
+ * Clase que permite operar sobre la cuenta bancaria de un cliente.
+ * 
+ * @author Juan Carlos Vilarrubia
+ * @version 1.0
+ */
 public class CCuenta {
 
 
+    /**
+     * Nombre del cliente propietario de la cuenta
+     */
     private String nombre;
+    /**
+     * Identificador de la cuenta del cliente
+    */
     private String cuenta;
+    /**
+     * Saldo actual en cuenta
+     */
     private double saldo;
-    // Eliminada la tilde del atributo
+    
+    /**
+     * Tipo de interes aplicado
+     */
     private double tipoInteres;
 
-
+    /**
+     * Constructor vacio de la clase
+     */
     public CCuenta()
     {
     }
-
+    
+    /**
+     * Contructor de la clase
+     * 
+     * @param nom Nombre del cliente propipetario de la cuenta
+     * @param cue Identificador de la cuenta
+     * @param sal Saldo inicial de la cuenta
+     * @param tipo Tipo de interés aplicado a la cuenta
+     */
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -29,30 +56,55 @@ public class CCuenta {
         tipoInteres = tipo;
     }
 
-    public void asignarNombre(String nom)
-    {
+    /**
+     * Establece el nombre del cliente propietario de la cuenta
+     * 
+     * @param nom Nombre del cliente
+     * @see CCuenta#setNombre(java.lang.String)  
+     */
+    public void asignarNombre(String nom) {
         setNombre(nom);
     }
-
-    public String obtenerNombre()
-    {
+    
+    /**
+     * Obtine el nombre del cliente propieterio de la cuenta
+     * 
+     * @return El nombre del cliente
+     * @see CCuenta#getNombre() 
+     */
+    public String obtenerNombre() {
         return getNombre();
     }
-
-
-    public double estado()
-    {
-        return getSaldo();
+    
+    /**
+     * Obtiene el saldo de la cuenta
+     * 
+     * @return El saldo de la cuenta
+     */
+    public double estado() {
+        return saldo;
     }
-
-
+    
+    /**
+     * Incrementa el saldo de la cuenta con la cantidad pasada como parámetro
+     * 
+     * @param cantidad Cantidad a ingresar en la cuenta
+     * @throws Exception Si la cantidad a ingresar es negativa
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
             throw new Exception("No se puede ingresar una cantidad negativa");
         saldo = getSaldo() + cantidad;
     }
-
+    
+    /**
+     * Decrementa el saldo en la cantidad pasada como parámetro
+     * 
+     * @param cantidad Cantidad a retirar de la cuenta
+     * @throws Exception Si la cantidad a retirar es negativa o supera el saldo 
+     * existente en cuenta
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
@@ -62,46 +114,68 @@ public class CCuenta {
         saldo = getSaldo() - cantidad;
     }
 
-
+    /**
+     * Obtiene el identificador de la cuenta
+     * 
+     * @return El identificador de la cuenta
+     * @see CCuenta#getCuenta()
+     */
     public String obtenerCuenta()
     {
         return getCuenta();
     }
 
-
+    /**
+     * Obtiene el nombre del cliente propietario de la cuenta
+     * 
+     * @return El nombre del cliente
+     */
     public String getNombre() {
         return nombre;
     }
 
-
+    /**
+     * Establece el nombre del cliente propietario de la cuenta
+     * 
+     * @param nombre El nombre del cliente
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
 
     /**
-     * @return the cuenta
+     * Obtiene el identificador de la cuenta
+     * 
+     * @return Identificador de la cuenta
+     * @see CCuenta#getCuenta()
      */
     public String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @return the saldo
+     * Obtiene el saldo de la cuenta
+     * 
+     * @return El saldo
      */
     public double getSaldo() {
         return saldo;
     }
 
     /**
-     * @return the tipoInteres
+     * Obtiene el tipo de interés aplicado a la cuenta
+     * 
+     * @return El tipo de Interés
      */
     public double getTipoInteres() {
         return tipoInteres;
     }
 
     /**
-     * @param tipoInteres the tipoInteres to set
+     * Establece el tipo de interés
+     * 
+     * @param tipoInteres El tipo de Interes a aplicar
      */
     public void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
